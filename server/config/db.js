7cbacf0171a3d8da376a95c8a5f34d73fba09f9a
Nusuwuba <nusaybazaman3@gmail.com>
2025-12-09 00:53:51 +0600

    DB connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,27 +1,39 @@
-import { MongoClient } from 'mongodb';
-import 'dotenv/config';
+import mongoose from "mongoose";
 
-const uri = process.env.MONGO_URI;
+const connectDB= async ()=>{
 
-let dbConnection;
+  mongoose.connection.on('connected', ()=>console.log("Successfully connected to Database uwu"));
 
-export const connectDB = async (cb) => {
-  try {
-    const client = new MongoClient(uri);
-    await client.connect();
+  await mongoose.connect(`${process.env.MONGODB_URI}/roommate-finder`);
+};
+
+export default connectDB;
+
+// import { MongoClient } from 'mongodb';
+// import 'dotenv/config';
+
+// const uri = process.env.MONGO_URI;
+
+// let dbConnection;
+
+// export const connectDB = async (cb) => {
+//   try {
+//     const client = new MongoClient(uri);
+//     await client.connect();
     
-    dbConnection = client.db(); 
-    console.log("Successfully connected to MongoDB Atlas!");
-    return cb();
-  } catch (err) {
-    console.error("Connection error:", err);
-    return cb(err);
-  }
-};
+//     dbConnection = client.db(); 
+//     console.log("Successfully connected to MongoDB Atlas!");
+//     return cb();
+//   } catch (err) {
+//     console.error("Connection error:", err);
+//     return cb(err);
+//   }
+// };
 
-export const getDb = () => {
-  if (!dbConnection) {
-    throw new Error("Database not initialized. Call connectDB first.");
-  }
-  return dbConnection;
-};
+// export const getDb = () => {
+//   if (!dbConnection) {
+//     throw new Error("Database not initialized. Call connectDB first.");
+//   }
+//   return dbConnection;
+// };
+
