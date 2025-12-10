a34460172ce839206f5793658ab31c891af1b314
Prachurzo07 <rayprachurzo02@gmail.com>
2025-12-11 04:32:46 +0600

    Add local userModel.js

---
+++ server/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {type: String, required: true},
+    email : {type: String, required: true, unique: true},
+    password : {type: String, required: true},
+    verifyOtp : {type: String, default: ''},
+    verifyOtpExpireAt : {type: Number, default: 0},
+    isAccountVerified : {type: Boolean, default: false},
+    resetOtp : {type: String, default: ''},
+    resetOtpExpireAt: {type:Number, default: 0},
+    
+    //Nusayba: From Class Diagram 
+    hobbies: { type: [String], default: [] },
+    age: { type: Number },
+    location: { type: String },
+    smoker: { type: Boolean },
+    personalityType: { type: String },
+    medicalConditions: { type: [String], default: [] },
+    institution: {type: [String], default: [] },
+    gender: { type: String},
+    visitors: { type: Boolean },
+    //Nusayba: From Class Diagram 
+
+})
+
+const userModel= mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModel;
+
