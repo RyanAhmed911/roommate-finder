99264ee0b161b35f8866b4e7a613f03f71331650
Prachurzo07 <rayprachurzo02@gmail.com>
2025-12-12 00:34:58 +0600

    login url updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,19 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login.jsx";
 import HomePage from "./pages/home.jsx";
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* redirect / to /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-
 
 export default App;
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000,   // 👈 run Vite on http://localhost:4000
+  },
 })
+
